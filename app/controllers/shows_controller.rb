21c1f3e1c575cf0dc17686add16f692e231d72e9
Devon Blandin <dblandin@gmail.com>
2012-01-12 05:12:39 -0600

    200

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -7,7 +7,7 @@ class ShowsController < ApplicationController
     respond_to do |format|
       format.html # index.html.erb
       #format.json { render json: @shows.to_json(:include=>[:people]) }
-      format.json{ :status => 200, :headers => ACCESS_CONTROL  }
+      format.json{}
       format.xml{}
     end
   
