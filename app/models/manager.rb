bfda7d17efb972d41fdb040085363edef0357bc0
Devon Blandin <dblandin@gmail.com>
2012-01-19 00:08:51 -0600

    thumb to small photo in manager model

--- app/models/manager.rb
+++ app/models/manager.rb
@@ -20,6 +20,6 @@ class Manager < ActiveRecord::Base
         :twitter => self.person.twitter_username,
         :linkedin => self.person.linkedin_username,
         :website => self.person.website_url,
-        :photo => self.person.avatar.thumb.url }
+        :photo => self.person.avatar.small.url }
   end
 end
