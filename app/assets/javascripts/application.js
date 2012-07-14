efdacae4b63a66091a49d9c295385e13df77ca31
Devon Blandin <dblandin@gmail.com>
2012-07-14 18:15:16 -0500

    table default length

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,6 @@
 //= require_tree .
 $('.datatable').dataTable({
   "sDom": "<'row-fluid'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>",
-  "sPaginationType": "bootstrap"
+  "sPaginationType": "bootstrap",
+  "iDisplayLength": 50
 });
