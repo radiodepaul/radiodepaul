8ec3dd99d3b04802e639133fd7e71b93b0b39a0e
Devon Blandin <dblandin@gmail.com>
2012-07-30 10:59:55 -0500

    form update

--- app/assets/javascripts/settings.js.coffee
+++ app/assets/javascripts/settings.js.coffee
@@ -1,3 +1,19 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(".ajax-save").click (event) ->
+  event.preventDefault()
+  setting = $(this).parent().siblings()[0]
+  setting = $(setting).children()[0]
+  setting = $(setting).attr('href')
+  console.log(setting)
+  $.ajax
+    url: setting
+    type: "PUT"
+    contentType: "application/json"
+    data: JSON.stringify(value: "SQ2012")
+    dataType: "json"
+    beforeSend: (xhr) ->
+      xhr.setRequestHeader "X-CSRF-Token", $("meta[name=\"csrf-token\"]").attr("content")
+    success: (data) ->
+      console.log data
--- app/controllers/slots_controller.rb
+++ app/controllers/slots_controller.rb
@@ -110,16 +110,14 @@ class SlotsController < ApplicationController
   
   
   def now_playing
-    
-      if Settings.override == "true"
-        @slot = Slot.find(Settings.override_show)
-      else
         current_day = Time.now.strftime("%A").downcase!
         @slot = Slot.find(:all, :conditions => ["quarter = ? AND start_time <= ? AND end_time >=  ? AND " + current_day + " = 't'", Settings.active_schedule, Time.now, Time.now])
         unless @slot.length > 0 then
           @slot = [{:show => { :title => "HAL 2012", :id => "10", :hosts => [{:name => "HAL 2012",:id => "58", :photo_thumb => "https://radiodepaul.s3.amazonaws.com/uploads/show/avatar/10/square_thumb_eee65920-911c-4f67-a746-2980cbfb4cc3.jpg"}], :genre => "Indie, College, Hip Hop", :short_description => "Hal 2011 is our automated system that plays when we don't have live shows. Hal is loaded with the newest and best tunes. Enjoy!", :photo => "https://radiodepaul.s3.amazonaws.com/uploads/show/avatar/10/square_small_eee65920-911c-4f67-a746-2980cbfb4cc3.jpg" }
           }]
         end
+      if (Settings.override == true)
+        @slot.first.show = Show.find(Settings.override_show)
       end
     respond_to do |format|
       format.js { render :json => @slot, :callback => params[:callback] }
@@ -143,15 +141,14 @@ class SlotsController < ApplicationController
   def getOnAir
     respond_to do |format|
       format.html { redirect_to pages_api_path }
-      if (Settings.override == true)
-        @slot = [Show.find(Settings.override_show)]
-      else
         current_day = Time.now.strftime("%A").downcase!
         @slot = Slot.find(:all, :conditions => ["quarter = ? AND start_time <= ? AND end_time >=  ? AND " + current_day + " = 't'", Settings.active_schedule, Time.now, Time.now])
         unless @slot.length > 0 then
           @slot = [{:show => { :title => "HAL 2012", :id => "10", :hosts => [{:name => "HAL 2012",:id => "58", :photo_thumb => "https://radiodepaul.s3.amazonaws.com/uploads/show/avatar/10/square_thumb_eee65920-911c-4f67-a746-2980cbfb4cc3.jpg"}], :genre => "Indie, College, Hip Hop", :short_description => "Hal 2011 is our automated system that plays when we don't have live shows. Hal is loaded with the newest and best tunes. Enjoy!", :photo => "https://radiodepaul.s3.amazonaws.com/uploads/show/avatar/10/square_small_eee65920-911c-4f67-a746-2980cbfb4cc3.jpg" }
           }]
         end
+      if (Settings.override == true)
+        @slot.first.show = Show.find(Settings.override_show)
       end
       format.js { render :json => @slot, :callback => params[:callback] }
       format.json  { render :json => @slot }
