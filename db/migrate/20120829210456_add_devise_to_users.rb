ec243285860226114af06e328d76bdceb85e06ac
Devon Blandin <dblandin@gmail.com>
2012-08-29 17:46:55 -0400

    fix migration

--- db/migrate/20120829210456_add_devise_to_users.rb
+++ db/migrate/20120829210456_add_devise_to_users.rb
@@ -1,5 +1,6 @@
 class AddDeviseToUsers < ActiveRecord::Migration
   def self.up
+    drop_table :users
     create_table(:users) do |t|
       t.string :first_name,         :null => false, :default => ""
       t.string :last_name,          :null => false, :default => ""
