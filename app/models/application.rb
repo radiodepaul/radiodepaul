ed9402ffe203d59ceb37cafa6cc123c005f5d0c7
Devon Blandin <dblandin@gmail.com>
2012-08-31 18:44:04 -0400

    add new application fields to attr_accessible

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,6 @@
 class Application < ActiveRecord::Base
   mount_uploader :avatar, AvatarUploader
-  attr_accessible :anything_else, :bio, :campus_involvement, :co_hosts, :depaul_id, :email, :experience, :famous_person, :favorite_artists, :favorite_films, :favorite_tv_shows, :first_name, :gpa, :home_city, :home_state, :host_type, :influences, :last_name, :major, :phone, :podcast_topic, :position, :show_description, :show_genres, :show_name, :show_type, :why_depaul, :year, :avatar, :created_at, :genre_list, :avatar_cache, :remote_avatar_url, :twitter_url, :facebook_url, :tumblr_url
+  attr_accessible :anything_else, :bio, :campus_involvement, :co_hosts, :depaul_id, :email, :experience, :famous_person, :favorite_artists, :favorite_films, :favorite_tv_shows, :first_name, :gpa, :home_city, :home_state, :host_type, :influences, :last_name, :major, :phone, :podcast_topic, :position, :show_description, :show_genres, :show_name, :show_type, :why_depaul, :year, :avatar, :created_at, :genre_list, :avatar_cache, :remote_avatar_url, :twitter_url, :facebook_url, :tumblr_url, :why_listen, :why_work_here
   acts_as_taggable_on :genres
   validates :first_name, :presence => true
   validates :last_name, :presence => true
