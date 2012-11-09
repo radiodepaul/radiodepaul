9f79809944d14f2067b7ecf5767abd0050d4e9fb
Devon Blandin <dblandin@gmail.com>
2012-11-09 03:39:19 -0600

    genres to string

--- app/models/show.rb
+++ app/models/show.rb
@@ -60,7 +60,7 @@ class Show < ActiveRecord::Base
   def as_json(options={})
        {id: self.id,
         title: self.title,
-        genres: self.genre_list,
+        genres: self.genre_list.join(", "),
         hosts: get_hosts,
         scheduled_slots: get_scheduled_slots,
         short_description: self.short_description || '',
